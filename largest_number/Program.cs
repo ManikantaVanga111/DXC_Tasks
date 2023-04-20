eff0108d88780671b86d8ada4b637970fee92bb9
Vanga <p.vanga@dxc.com>
2023-04-20 12:56:11 +0530

    updated on 20-3-2023

--- largest_number/Program.cs
+++ largest_number/Program.cs
@@ -5,10 +5,10 @@ Console.WriteLine("Enter the second number");
 int b = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine("Enter the third number");
 int c = Convert.ToInt16(Console.ReadLine());
-if(a>b)
+if(a>b && a>c)
 {
     Console.WriteLine("a is largest");
-}else if(b>c)
+}else if(b>a && b>c)
 {
     Console.WriteLine("b is largest");
 } else
