70037b9299fb46f0b02b6f7d61fe4f11339b1b2f
Vanga <p.vanga@dxc.com>
2023-04-20 12:33:08 +0530

    updated on 20-4-2023

---
+++ largest_number/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int b = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter the third number");
+int c = Convert.ToInt16(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("a is largest");
+}else if(b>c)
+{
+    Console.WriteLine("b is largest");
+} else
+{
+    Console.WriteLine("c is greater");
+}
+
---
+++ vowel _or_not/vowel _or_not/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Enter the character");
+char ap=Convert.ToChar(Console.ReadLine());
+if(ap == 'A' || ap =='a' || ap=='e' || ap=='E' ||ap=='i' || ap=='I' || ap=='o' ||ap=='O' ||ap =='u' || ap=='U' )
+{
+    Console.WriteLine("It's an vowel");
+} else 
+{
+    Console.WriteLine("It's not an vowel");
+}
+
