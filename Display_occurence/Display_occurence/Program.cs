4060071b480af665aca26009c24ce7bdf1af2a90
Vanga <p.vanga@dxc.com>
2023-04-25 08:32:14 +0530

    updated on 24-4-2023

--- Display_occurence/Display_occurence/Program.cs
+++ Display_occurence/Display_occurence/Program.cs
@@ -1,11 +1,11 @@
-﻿int[] arr = { 7, 7, 7, 7, 8, 8, 8, 5, 5, 5, 5, 6, 6, 6,6};
+﻿int[] arr = { 7, 7, 7, 7, 8, 8, 8, 5, 5, 5, 5, 6, 6, 6, 6 };
 int most = 0;
 int count = 0;
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < arr.Length; i++)
 {
     int most1 = arr[0];
     int count1 = 0;
-    for (int j = 0; j < 10; j++)
+    for (int j = 0; j < arr.Length; j++)
     {
         if (arr[j] == most1)
         {
