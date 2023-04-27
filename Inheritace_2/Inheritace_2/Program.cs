33b65bc1805b1f73d40208998f46383c2f3e92c0
Vanga <p.vanga@dxc.com>
2023-04-27 14:24:47 +0530

    updated

---
+++ Inheritace_2/Inheritace_2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Inheritace_2
+{
+    class Cal1
+    {
+        
+       // public Cal1(int x) { 
+           // Console.WriteLine(10);
+           // }
+           public Cal1()
+           {
+            Console.WriteLine("hi");
+           }
+        public Cal1(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public Cal1(int x)
+        {
+            Console.WriteLine(x);
+        }
+        public void show(int x, int y,int z)
+        {
+            Console.WriteLine(x+y+z);
+        }
+    }
+
+    class Cal2 : Cal1
+    {
+       public Cal2 () 
+        {
+            base.show(10,20,30);    
+            Console.WriteLine("Second one");
+        }
+        public Cal2(String x) :base(9,1)
+        {
+            Console.WriteLine(x);
+        }
+    }
+    class Cal3 :Cal2 
+    {
+        public Cal3()
+        {
+            Console.WriteLine("nothing");
+        }
+      public Cal3(int x, int y) :base("hi")
+       {
+         Console.WriteLine(x + y);
+       }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello, World!");
+          // Cal1 sc= new Cal1(10); 
+         // Cal2 sc= new Cal2();
+
+            Cal3 sc=new Cal3(1,4);
+        }
+    }
+}
---
+++ Inheritance_1/Inheritance_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Inheritance_1
+{
+    class Calculator{
+        public Calculator()
+        {
+            Console.WriteLine("hi constructor");
+        }
+
+    }
+    class Cal1 : Calculator
+    {
+        public Cal1()
+        {
+            Console.WriteLine("hi second  constructor");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Calculator sc=new Calculator();
+          Cal1 sc= new Cal1();  
+        }
+    }
+}
